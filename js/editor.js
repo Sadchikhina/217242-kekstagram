a2e2d992fe1655fddc97891744697b04faee9cc2
Varvara <sadchikhina@gmail.com>
2018-02-20 12:23:21 +0300

    modulation in progress

---
+++ js/editor.js
@@ -0,0 +1,62 @@
+'use strict';
+
+(function () {
+
+  var inputUpload = document.querySelector('#upload-file');
+  var uploadForm = document.querySelector('.upload-form');
+  var editor = document.querySelector('.upload-overlay');
+  var editorClose = editor.querySelector('.upload-form-cancel');
+
+  var ESC_KEYCODE = 27;
+
+  var showForm = function () {
+
+    /**
+     * Закрывает окно по нажатию на ESC
+     * @param  {type} evt
+     */
+    var openEditorHandler = function (evt) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        closeEditor();
+      }
+    };
+
+    /**
+   * Открывает окно, закрывает по нажатию на ESC
+    */
+    var openEditor = function () {
+      editor.classList.remove('hidden');
+      document.addEventListener('keydown', openEditorHandler);
+    };
+
+    /**
+     * Закрытие формы
+     */
+    var closeEditor = function () {
+      editor.classList.add('hidden');
+      editor.removeEventListener('keydown', openEditorHandler);
+      uploadForm.reset();
+    };
+
+    inputUpload.addEventListener('change', function () {
+      openEditor();
+    });
+
+    editorClose.addEventListener('click', function () {
+      closeEditor();
+    });
+  };
+
+  showForm();
+
+  /**
+   * Отправка формы
+   */
+  var commentField = editor.querySelector('.upload-form-description');
+
+  commentField.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      evt.stopPropagation();
+    }
+  });
+})();
