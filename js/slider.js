0553fa4c9afed343b1498b084824c69e0ed08562
Varvara <sadchikhina@gmail.com>
2018-02-21 12:25:52 +0300

    slider in progress

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,44 @@
 'use strict';
 
-/*
- var effectControls = document.querySelector('.upload-effect-controls');
- var effectLevel = document.querySelector('.upload-effect-level');
- var sliderPin = document.querySelector('.upload-effect-level-pin');
- var effectImagePreview = document.querySelector('.upload-effect-level-pin');
+var slider = document.querySelector('.upload-effect-level-line');
+var sliderPin = document.querySelector('.upload-effect-level-pin');
 
- sliderPin.addEventListener('mousedown', function (evt) {
+sliderPin.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
-  var startCoords = {
-    x: evt.clientX
+  var sliderPinCoords = getCoords(sliderPin);
+  var shiftX = evt.pageX - sliderPinCoords.left;
+
+  var sliderCoords = getCoords(slider);
+
+  var mouseMoveHandler = function (moveEvt) {
+
+    var leftEdge = moveEvt.pageX - shiftX - sliderCoords.left;
+    if (leftEdge < 0) {
+      leftEdge = 0;
+    }
+
+    var rightEdge = slider.offsetWidth;
+    if (leftEdge > rightEdge) {
+      leftEdge = rightEdge;
+    }
+
+    sliderPin.style.left = leftEdge + 'px';
+
   };
-  var onMouseMove = function (moveEvt) {
-    var shift = {
-      x: startCoords.x - moveEvt.clientX
-    };
-    startCoords = {
-      x: moveEvt.clientX
-    };
-    sliderPin.style.left = (sliderPin.offsetLeft - shift.x) + 'px';
-  };
-  var onMouseUp = function (upEvt) {
+  var mouseUpHandler = function (upEvt) {
     upEvt.preventDefault();
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
   };
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+
 });
-*/
+
+var getCoords = function (elem) {
+  var box = elem.getBoundingClientRect();
+
+  return {
+    left: box.left + pageXOffset
+  };
+};
