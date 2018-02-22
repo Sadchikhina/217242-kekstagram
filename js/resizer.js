6c4d42166268b627b855ec7b6b04f1d4003399ce
Varvara <sadchikhina@gmail.com>
2018-02-22 12:02:48 +0300

    corrected

--- js/resizer.js
+++ js/resizer.js
@@ -7,33 +7,31 @@
   var increasePicture = resizeControls.querySelector('.upload-resize-controls-button-inc');
   var resizeValue = resizeControls.querySelector('.upload-resize-controls-value');
   var imagePreview = document.querySelector('.effect-image-preview');
-  resizeValue.value = '100';
 
+  var MAX = 100;
+  var MIN = 25;
+  var STEP = 25;
+
+  resizeValue.value = 100;
 
   decreasePicture.addEventListener('click', function () {
-    if (resizeValue.value >= 100) {
-      resizeValue.value = resizeValue.value - 25;
-      imagePreview.style.transform = 'scale(0.75)';
-    } else if (resizeValue.value >= 75) {
-      resizeValue.value = resizeValue.value - 25;
-      imagePreview.style.transform = 'scale(0.5)';
-    } else if (resizeValue.value >= 50) {
-      resizeValue.value = resizeValue.value - 25;
-      imagePreview.style.transform = 'scale(0.25)';
+    var decreasedValue = Number(resizeValue.value) - STEP;
+    if (decreasedValue < MIN) {
+      decreasedValue = MIN;
     }
+    var decreasedScale = decreasedValue / 100;
+    resizeValue.value = decreasedValue;
+    imagePreview.style.transform = 'scale(' + decreasedScale + ')';
   });
 
   increasePicture.addEventListener('click', function () {
-    if (resizeValue.value <= 25) {
-      resizeValue.value = resizeValue.value + 25;
-      imagePreview.style.transform = 'scale(0.5)';
-    } else if (resizeValue.value <= 50) {
-      resizeValue.value = resizeValue.value + 25;
-      imagePreview.style.transform = 'scale(0.75)';
-    } else if (resizeValue.value <= 75) {
-      resizeValue.value = resizeValue.value + 25;
-      imagePreview.style.transform = 'scale(1)';
+    var increasedValue = Number(resizeValue.value) + STEP;
+    if (increasedValue > MAX) {
+      increasedValue = MAX;
     }
+    var increasedScale = increasedValue / 100;
+    resizeValue.value = increasedValue;
+    imagePreview.style.transform = 'scale(' + increasedScale + ')';
   });
 
 })();
